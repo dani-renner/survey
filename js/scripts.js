2634b9b522cb301e7163d59cbf2637634930751a
Dani Renner <danijrenner@gmail.com>
2021-01-13 15:33:23 -0800

    using all forms we have learned
    Co-authored-by : Glen Buck <glenbuck503@gmail.com>
    Co-authored-by : Scott O'Neil <scottieoneil@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#formOne").submit(function(event) {
+      event.preventDefault();
+      const person1Input = $("input#person1").val();
+      const personFood = $("input:radio[name=food]:checked").val();
+      const personBeverage = $("#beverage").val();
+      const dob = $("#born").val();
+      const color = $("#color").val();
+
+      $(".userName").text(person1Input);
+      $(".userFood").text(personFood);
+      $(".userBeverage").text(personBeverage);
+      $(".userDob").text(dob);
+      $(".userColor").text(color);
+      $(".output").fadeIn();
+    });
+  });
